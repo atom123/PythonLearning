14554f3aa796a83ec66ddc06458697b70c9cb994
CV0033106 <double12gzh@icloud.com>
2015-07-13 21:53:01 +0800

    add a function to save the Loin list into a file

--- getCellValue.py
+++ getCellValue.py
@@ -3,13 +3,18 @@
 
 import sys
 import xlrd
+from xml.etree import ElementTree as ET
 
-def open_excel(file='file.xls'):
+######################################################################
+#	open an excel
+######################################################################
+def open_excel(inputFile='file.xls'):
 	try:
-		wb = xlrd.open_workbook(file)
+		wb = xlrd.open_workbook(inputFile)
 		return wb
 	except Excetion,e:
 		print(str(e))
+
 #######################################################################
 #	get the value of special cells.
 #   the output is as follows:
@@ -17,8 +22,8 @@ def open_excel(file='file.xls'):
 #	{'Administrator': 'Alcatel&01'}
 #	{'Webportal': 'Administrator2.'}
 #######################################################################
-def excel_table_byname1(file= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
-	wb = open_excel(file)
+def excel_table_byname1(inputFile= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
+	wb = open_excel(inputFile)
 	ws = wb.sheet_by_name(sheet_name)
 	nrows = ws.nrows # rows of the sheet
 
@@ -38,8 +43,8 @@ def excel_table_byname1(file= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
 #	Output for list1
 #	[{'Administrator': 'Alcatel&01'}, {'Webportal': 'Administrator2.'}]
 ##################################################################################
-def excel_table_byname2(file= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
-	wb = open_excel(file)
+def excel_table_byname2(inputFile= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
+	wb = open_excel(inputFile)
 	ws = wb.sheet_by_name(sheet_name)
 	nrows = ws.nrows # rows of the sheet
 
@@ -55,6 +60,62 @@ def excel_table_byname2(file= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
 	return list1
 
 ################################################################################
+#	Function:		GenLoginFile
+#
+#	Description:	Create a tree for Login file. 	
+#
+#					The generated Login File is:
+#
+#	<?xml version='1.0' encoding='utf8'?>
+#	<Request Action="LOGIN" RequestID="100000">
+#		<Authentication>
+#			<ClientName>Administrator</ClientName>
+#			<Password>Alcatel&01</Password>
+#		</Authentication>
+#	</Request>
+#
+#	Input:			Login	-	an input dict, the "ClientName" is used as the Key
+#								"Password" is used as the value.
+#					Flag	-	 Flag to indicate to get FSDB or GLS Login File.
+#
+#									1 - get FSDB Login File
+#									2 - get GLS Login File
+#
+#	Output:			NONE				
+#			
+################################################################################
+def GenLoginFile(Login, Flag):
+
+	# Create a root for the tree.
+	root = ET.Element("Request", {"Action": "LOGIN", "RequestID": "100000"})
+
+	# Create a sub tree. authSubAttrib is the subroot for usrSubAttrib and 
+	# passwdSubAttrib. usrSubAttrib and passwdSubAttrib are on the same level.
+	authSubAttrib = ET.Element("Authentication")
+
+	usrSubAttrib = ET.SubElement(authSubAttrib, "ClientName")
+	usrSubAttrib.text = Login["ClientName"]
+
+	passwdSubAttrib = ET.SubElement(authSubAttrib, "Password" )
+	passwdSubAttrib.text = Login["Password"]
+
+	# "root" is the root for the LoginFile tree.
+	root.append(authSubAttrib)
+	tree = ET.ElementTree(root)
+	
+	# Besides FSDB and GLS, if another elements' Login File needs to be got, just
+	# add "elif" check and set the value of "LoginName".
+	if Flag == 1:
+		LoginName = "fsdb"
+
+	elif Flag == 2:
+		LoginName = "gls"
+
+	# Save this tree in a file.
+	tree.write("LoginFile" + LoginName + ".xml", "utf8")
+		
+
+################################################################################
 #	Function Name:	getLogin
 #
 #	Decsription:	Do "Role" check to find the "ADMINISTRATOR", then check the 
@@ -62,21 +123,23 @@ def excel_table_byname2(file= 'file.xls',rowNameIndex=0,sheet_name=u'Sheet'):
 # 					By "LOGIN" and "ADMINISTRATOR",	we can easily locate a cell 
 # 					for "ClientName" and "Password" separatly.
 #
-#	Inputs:			filename	-	The file contained "ClientName" and "Password".
-# 									In our scenario, the file is CTSTemplates.xls.
-#					sheet_name	-	worksheet name.	
+#	Inputs:			inputWorkBook	-	The file contained "ClientName" and "Password".
+# 										In our scenario, the file is CTSTemplates.xls.
+#					sheet_name		-	worksheet name.	
 #
-#	Output:			Login		-	a dict to save "ClientName" and "Password".
+#	Output:			Login			-	a dict to save "ClientName" and "Password".
+#										if not found, then just return {}
 ################################################################################
-def getLogin(filename, sheet_name=u'Sheet'): 
+def getLogin(inputWorkBook, sheet_name='Sheet'): 
 
 	Login = {}			# to save usrname and passwd.
 	titleColOrder = {}	# to save the title column order.	
+
+	wb = open_excel(inputWorkBook)			# open a excel and return a Book object.
+	ws = wb.sheet_by_name(sheet_name)		# worksheet got by input sheet_name.
+
 	nrows = ws.nrows 	# rows of the sheet.
 	ncols = ws.ncols 	# column of this sheet.
-
-	wb = open_excel(filename)				# open a excel and return a Book object.
-	ws = wb.sheet_by_name(sheet_name)		# worksheet got by input sheet_name.
 
 	# the value of the rowNameIndex row.
 	rowValuesList = ws.row_values(0) 
@@ -86,34 +149,37 @@ def getLogin(filename, sheet_name=u'Sheet'):
 	 
 	for num_element in range(0, len(rowValuesList)):
 
-		if rowValuesList[num_element] == u'XML_ACTION':
+		if rowValuesList[num_element] == 'XML_ACTION':
 			# Column order for "XML_ACTION"
-			titleColOrder[u'XML_ACTION'] = num_element
+			titleColOrder['XML_ACTION'] = num_element
 
-		elif rowValuesList[num_element] == u'ClientName':
+		elif rowValuesList[num_element] == 'ClientName':
 			# Column order for "ClientName"
-			titleColOrder[u'ClientName'] = num_element
+			titleColOrder['ClientName'] = num_element
 
-		elif rowValuesList[num_element] == u'Password':
+		elif rowValuesList[num_element] == 'Password':
 			# Column order for "Password"
-			 titleColOrder[u'Password'] = num_element
+			 titleColOrder['Password'] = num_element
 
-		elif rowValuesList[num_element] == u'Role':
+		elif rowValuesList[num_element] == 'Role':
 			# Column order for "Role"
-			titleColOrder[u'Role'] = num_element
+			titleColOrder['Role'] = num_element
 		
 	print(titleColOrder)
 
 	try:
 		# Check the "Role" column to find the "ADMINISTRATOR" role.
-		for i in range(0, titleColOrder[u'Role']):
-			if ws.cell(i, titleColOrder[u'Role'])).value == u'ADMINISTRATOR':
-				if ws.cell(i, titleColOrder[u'XML_ACTION']).value == u'LOGIN':
-					ClientName = ws.cell(i,titleColOrder[u'ClientName']).value
-					Password = ws.cell(i,titleColOrder[u'Password']).value
+		for i in range(0, titleColOrder['Role']):
+			if ws.cell(i, titleColOrder['Role']).value == 'ADMINISTRATOR':
+				if ws.cell(i, titleColOrder['XML_ACTION']).value == 'LOGIN':
+					ClientName = str(ws.cell(i,titleColOrder['ClientName']).value).strip()
+					Password = str(ws.cell(i,titleColOrder['Password']).value).strip()
 
-					Login[u'ClintName'] = ClientName
-					Login[u'Password'] = Password
+					Login['ClientName'] = ClientName
+					Login['Password'] = Password
+					
+					# Generate the Login file.
+					GenLoginFile(Login, 1)
 
 					return Login	# Only one "ADMISTRATOR" ClientName and 
 									# Password is needed. Therefore, if found,
@@ -121,7 +187,7 @@ def getLogin(filename, sheet_name=u'Sheet'):
 		raise StopIteration
 		
 	except StopIteration:
-		print("\n No 'ADMISTRATOR' Login are found")
+		print("\n No 'ADMISTRATOR' Login are found or XML_ACTION is not 'LOGIN'")
 		return {}
 
 if __name__ == "__main__":
@@ -155,6 +221,6 @@ if __name__ == "__main__":
 		#print(tables[i])
 
 #03 "Role" check
-	getLogin(filename, u'ClientAdmin-fsdb0')
+	getLogin(filename, 'ClientAdmin-fsdb0')
 
 
